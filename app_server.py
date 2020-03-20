c6058d487f2d13e4bbdd85c3b21d6a9b29ab5bba
tony363 <pysolver33@gmail.com>
2020-03-20 17:24:21 +0800

    got something working

---
+++ app_server.py
@@ -0,0 +1,54 @@
+import time
+import socket
+import types
+import sys
+import selectors
+
+# used = list()
+
+def accept_wrapper(sock):
+    conn, addr = sock.accept()  # Should be ready to read
+    print('accepted connection from', addr)
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b'', outb=b'')
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Should be ready to read
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print('closing connection to', data.addr)
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print('echoing', repr(data.outb), 'to', data.addr)
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
+
+
+
+
+sel = selectors.DefaultSelector()
+lsock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+lsock.bind(('',8000))
+lsock.listen()
+print('listening on',('',8000))
+
+lsock.setblocking(False)
+sel.register(lsock,selectors.EVENT_READ,data=None)
+
+
+
+while True:
+    events = sel.select(timeout=None)
+    for key, mask in events:
+        if key.data is None:
+            accept_wrapper(key.fileobj)
+        else:
+            service_connection(key, mask)
