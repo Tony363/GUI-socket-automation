293138b05b0919a93aa797a2031cd99d8faf393f
tony363 <pysolver33@gmail.com>
2020-04-16 17:37:56 +0800

    still gotta make this an executable

--- random_gen.py
+++ random_gen.py
@@ -62,8 +62,36 @@ def start_connections(host, port, num_conns,sel,messages):
         data1 = json.dumps(messages).encode('utf-8')
         sel.register(sock, events, data=data1)
 
+class deleter:
+    def __init__(self,root):
+        self.root = root
+        self.root.geometry("300x200")
+
+        self.data = {'number':[],'label1':[],'label2':[]}
+        self.delete_label = Label(root,text='Number to Delete')
+        self.delete_label.pack()
+        
+        self.delete_prompt = Entry(root)
+        self.delete_prompt.pack()
+
+        self.delete_button = Button(root,text='delete record',command=self.delete)
+        self.delete_button.pack()
+
+    def delete(self):
+        self.data['number'].append(self.delete_prompt.get())
+        url = 'http://146.148.79.188/delete/'
+        r = requests.post(url,json=self.data)
+        print(r.content)
+        print(r.status_code)
+        if r.status_code == 404:
+            showinfo('Repeating','data already processed') 
+
+        time.sleep(1)
+        self.root.destroy()
+        
+
 class editor:
-    def __init__(self,root=Tk()):
+    def __init__(self,root):
         self.root = root
         self.root.geometry("300x300+200+200")
 
@@ -87,12 +115,6 @@ class editor:
         self.Entry3 = Entry(root)
         self.Entry3.pack()
 
-        # self.label4 = Label(master,text='label3')
-        # self.label4.grid()
-
-        # self.Entry4 = Entry(master)
-        # self.Entry4.grid()
-
         self.update = Button(root,text = 'Update Record', command = self.update_data )
         self.update.pack()
 
@@ -105,7 +127,7 @@ class editor:
         self.data['label2'].append(self.Entry3.get())
         print(self.data)
 
-        url = 'http://127.0.0.1:8000/update/'
+        url = 'http://146.148.79.188/update/'
         r = requests.post(url,json=self.data)
         print(r.content)
         print(r.status_code)
@@ -121,14 +143,11 @@ class editor:
 
 
 
+class GUI:
+    def __init__(self,root):
+        self.root = root
+        self.root.geometry("300x300+200+200")
 
-
-
-class GUI(Frame):
-    def __init__(self,master=None):
-        Frame.__init__(self,master)
-        
-        self.grid()
         self.counter = 1000
         self.data = {'number':[],'label1':[],'label2':[]}
         # self.messages = [bytes(f'{i}',encoding='utf8') for i in range(1000,100000)] 
@@ -137,43 +156,37 @@ class GUI(Frame):
         # self.sel = selectors.DefaultSelector()
         # start_connections('',8000,1,self.sel,self.messages)        
 
-        self.label1 = Label(master,text='label1')
-        self.label1.grid()
+        self.label1 = Label(self.root,text='label1')
+        self.label1.pack()
 
-        self.Entry1 = Entry(master)
-        self.Entry1.grid()
+        self.Entry1 = Entry(self.root)
+        self.Entry1.pack()
 
-        self.label2 = Label(master,text='label2')
-        self.label2.grid()
+        self.label2 = Label(self.root,text='label2')
+        self.label2.pack()
 
-        self.Entry2 = Entry(master)
-        self.Entry2.grid()
+        self.Entry2 = Entry(self.root)
+        self.Entry2.pack()
 
-        self.submit = Button(text = f'Submit {self.counter}', command = self.send_data )
-        self.submit.grid()
+        self.submit = Button(self.root,text = f'Submit {self.counter}', command = self.send_data )
+        self.submit.pack()
 
-        self.download_csv = Button(text = 'Download', command = self.download)
-        self.download_csv.grid()
+        self.download_csv = Button(self.root,text = 'Download', command = self.download)
+        self.download_csv.pack()
 
-        self.edit = Button(text = 'edit', command = lambda: self.edit_records(editor) )
-        self.edit.grid()
+        self.edit = Button(self.root,text = 'edit', command = lambda: self.edit_records(editor) )
+        self.edit.pack()
+
+        self.delete = Button(self.root,text='delete',command=lambda:self.delete_records(deleter))
+        self.delete.pack()
+
+    def delete_records(self,_class):
+        self.delete = Toplevel(self.root)
+        _class(self.delete)
 
     def edit_records(self,_class):
-        self.edit = Toplevel()
+        self.edit = Toplevel(self.root)
         _class(self.edit)
-
-
-    def get_entry1(self):
-        self.messages[self.counter]['label1'] = self.Entry1.get()
-        self.data['label1'].append(self.Entry1.get())
-        
-        print(self.data)
-
-    def get_entry2(self):
-        self.messages[self.counter]['label2'] = self.Entry2.get()
-        self.data['label2'].append(self.Entry2.get())
-        
-        print(self.data)
                
 
     def nClick(self):
@@ -193,19 +206,18 @@ class GUI(Frame):
         return self.messages
 
     def send_data(self):
-        self.counter += 1
         self.data['number'].append(self.counter)
         self.data['label1'].append(self.Entry1.get())
         self.data['label2'].append(self.Entry2.get())
         print(self.data)
   
         if self.counter > 100000:
-            stop = Label(text="exceeded 5 digits").pack()
+            stop = Label(text="exceeded 5 digits").grid()
             time.sleep(1)
             sys.exit() 
             
-        # url = 'http://34.84.220.35/data/'
-        url = 'http://127.0.0.1:8000/data/'
+        url = 'http://146.148.79.188/data/'
+        # url = 'http://127.0.0.1:8000/data/'
         r = requests.post(url,json=self.data)
         print(r.content)
         print(r.status_code)
@@ -219,8 +231,8 @@ class GUI(Frame):
         self.data['label2'].pop(0)
     
     def download(self):
-        # url = 'http://34.84.220.35/feed_data/'
-        url = 'http://127.0.0.1:8000/feed_data'
+        url = 'http://146.148.79.188/feed_data/'
+        # url = 'http://127.0.0.1:8000/feed_data'
         r = requests.post(url)
         data = dict(r.json())
         print(data['data so far'])
@@ -229,9 +241,13 @@ class GUI(Frame):
         desktop = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop') 
         df.to_csv(desktop + '/your_data.csv')
  
+if __name__ == "__main__":
+    root = Tk()
+    app = GUI(root)
+    app.root.title('csv automation')
+    root.mainloop()
+      
 
-guiFrame = GUI()   
-guiFrame.mainloop()
         
    
 
